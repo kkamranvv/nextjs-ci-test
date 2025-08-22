db925546bb3d21f6abc9f25b142c40bb9dc7d068
Kamran <musadirlikamran@gmail.com>
2025-08-22 13:00:41 +0200

    init project with CI

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+import { i18n } from "./next-i18next.config";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   reactStrictMode: true,
+  i18n,
 };
 
 export default nextConfig;
